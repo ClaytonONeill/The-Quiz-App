bb27cadc0e997b4508f5828ff976c89f7146e248
Courtney Zavaleta <courtneyzavaleta@gmail.com>
2020-04-18 01:00:31 +0100

    merge

--- src/components/Form.js
+++ src/components/Form.js
@@ -71,63 +71,41 @@ class Form extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
       <div className="forms">
-      Title:
         <input className="formTitle" type="text" placeholder="Title of Quiz" id="quiztitle"  value={this.state.quiztitle} onChange={this.handleChange}/><br/>
-      Category:
         <input className="formCategory" type="text" placeholder="Category" id="category" value={this.state.category} onChange={this.handleChange}/><br/>
 
       <div className="q1">
-      Question 1:
           <input type="text" placeholder="Question 1" id="question1" value={this.state.question1} onChange={this.handleChange}/><br/>
-      Correct Answer:
           <input type="text" placeholder="Correct Answer" id="answer1" value={this.state.answer1} onChange={this.handleChange}/><br/>
-      Wrong Answer:
           <input type="text" placeholder="Wrong Answer" id="wronganswer1a" value={this.state.wronganswer1a} onChange={this.handleChange}/><br/>
-      Wrong Answer:
           <input type="text" placeholder="Wrong Answer" id="wronganswer1b" value={this.state.wronganswer1b} onChange={this.handleChange}/><br/>
       </div>
 
       <div className="q2">
-      Question 2:
           <input type="text" placeholder="Question 2" id="question2" value={this.state.question2} onChange={this.handleChange}/><br/>
-      Correct Answer:
           <input type="text" placeholder="Correct Answer" id="answer2" value={this.state.answer2} onChange={this.handleChange}/><br/>
-      Wrong Answer:
           <input type="text" placeholder="Wrong Answer" id="wronganswer2a" value={this.state.wronganswer2a} onChange={this.handleChange}/><br/>
-      Wrong Answer:
           <input type="text" placeholder="Wrong Answer" id="wronganswer2b" value={this.state.wronganswer2b} onChange={this.handleChange}/><br/>
       </div>
 
       <div className="q3">
-      Question 3:
           <input type="text" placeholder="Question 3" id="question3" value={this.state.question3} onChange={this.handleChange}/><br/>
-      Correct Answer:
               <input type="text" placeholder="Correct Answer" id="answer3" value={this.state.answer3} onChange={this.handleChange}/><br/>
-      Wrong Answer:
           <input type="text" placeholder="Wrong Answer" id="wronganswer3a" value={this.state.wronganswer3a} onChange={this.handleChange}/><br/>
-      Wrong Answer:
           <input type="text" placeholder="Wrong Answer" id="wronganswer3b" value={this.state.wronganswer3b} onChange={this.handleChange}/><br/>
       </div>
 
       <div className="q4">
-      Question 4:
           <input type="text" placeholder="Question 4" id="question4" value={this.state.question4} onChange={this.handleChange}/><br/>
-      Correct Answer:
           <input type="text" placeholder="Correct Answer" id="answer4" value={this.state.answer4} onChange={this.handleChange}/><br/>
-      Wrong Answer:
           <input type="text" placeholder="Wrong Answer" id="wronganswer4a" value={this.state.wronganswer4a} onChange={this.handleChange}/><br/>
-      Wrong Answer:
           <input type="text" placeholder="Wrong Answer" id="wronganswer4b" value={this.state.wronganswer4b} onChange={this.handleChange}/><br/>
       </div>
 
       <div className="q5">
-      Question 5:
           <input type="text" placeholder="Question 5" id="question5" value={this.state.question5} onChange={this.handleChange}/><br/>
-      Correct Answer:
           <input type="text" placeholder="Correct Answer" id="answer5" value={this.state.answer5} onChange={this.handleChange}/><br/>
-      Wrong Answer:
           <input type="text" placeholder="Wrong Answer" id="wronganswer5a" value={this.state.wronganswer5a} onChange={this.handleChange}/><br/>
-      Wrong Answer:
           <input type="text" placeholder="Wrong Answer" id="wronganswer5b" value={this.state.wronganswer5b} onChange={this.handleChange}/><br/>
       </div>
 
